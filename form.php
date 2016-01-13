bb85f0fb3b6483031d985f1c14de7339687dd7c8
Radhamst <radha@mstsolutions.com>
2016-01-13 16:54:18 +0530

    mst name changed

--- form.php
+++ form.php
@@ -34,9 +34,9 @@ return false;
 
 <form method="post" name="form">
 <ul><li>
-<input id="mstnamee" name="name" type="text" />
+<input id="name" name="name" type="text" />
 </li><li>
-<input id="mstusernamee" name="username" type="text" />
+<input id="username" name="username" type="text" />
 </li>
 <!--
 <li>
